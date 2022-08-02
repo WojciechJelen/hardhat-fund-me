ef7525f128dbdf75f4759e8b2c6c35d8b9112ce4
Wojciech Jelen <w.jelen20@gmail.com>
2022-08-02 16:57:03 +0200

    Added config and contracts

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-solhint'
+import 'hardhat-deploy'
+
+const config: HardhatUserConfig = {
+    solidity: '0.8.9',
+}
+
+export default config
