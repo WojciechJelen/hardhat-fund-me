4de30c783f92c3ea4ded9411774c72e921276e6c
Wojciech Jelen <w.jelen20@gmail.com>
2022-08-06 10:16:44 +0200

    fund tests

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -19,4 +19,42 @@ describe('FundMe', () => {
       expect(res).to.equal(mockV3aggregator.address)
     })
   })
+
+  describe('fund', async function () {
+    it('Fails if you dont send enought ETH', async function () {
+      const amount = ethers.utils.parseEther('0.1')
+      await expect(fundMe.fund()).to.be.revertedWith(
+        'You need to spend more ETH!',
+      )
+    })
+
+    it('Funds the contract if you send enought ETH', async function () {
+      const amount = ethers.utils.parseEther('0.1')
+      await expect(
+        fundMe.fund({
+          value: amount,
+        }),
+      ).to.be.fulfilled
+    })
+
+    it('Updates addressToAmountFunded properly', async function () {
+      const amountSent = ethers.utils.parseEther('0.1')
+      await fundMe.fund({
+        value: amountSent,
+      })
+      const addressToAmountFunded = await fundMe.addressToAmountFunded(deployer)
+      expect(addressToAmountFunded.toString()).to.be.equal(
+        amountSent.toString(),
+      )
+    })
+
+    it('Adds funders to funders array', async function () {
+      const amountSent = ethers.utils.parseEther('0.1')
+      await fundMe.fund({
+        value: amountSent,
+      })
+      const funder = await fundMe.funders(0)
+      expect(funder).to.be.equal(deployer)
+    })
+  })
 })
